2db01bcb770024fa96601902c7b43e9e68fa0698
Jerome Chery <jerome.chery@nrco.fr>
2018-04-16 15:25:33 +0200

    Affiche fiche

--- src/js/services/template.js
+++ src/js/services/template.js
@@ -1,5 +1,5 @@
 angular
-    .module('KMS')
+	.module('KMS')
 	.factory('Template', ['$http', '$q', 'FB', function ($http, $q, FB) {
 		var svc = {
 			Datas: {
@@ -15,25 +15,32 @@ angular
 
 		svc.ViewBox = function () { return "0 0 " + svc.Datas.Fond.Width + " " + svc.Datas.Fond.Height; }
 
-        svc.GetCopy = function (id) {
-            return $q(function (resolve, reject) {
-                var datas = FB.GetObject('templates/' + id);
-                datas.$loaded().then(function () {
-                    angular.copy(datas, svc.Datas);
-                    delete svc.Datas["$$conf"];
-                    delete svc.Datas["$id"];
-                    delete svc.Datas["$value"];
-                    delete svc.Datas["$priority"];
-                    delete svc.Datas["$resolved"];
-                    resolve(svc);
-                })
-            });
-        }
+		svc.GetCopy = function (id) {
+			return $q(function (resolve, reject) {
+				var datas = FB.GetObject('templates/' + id);
+				datas.$loaded().then(function () {
+					angular.copy(datas, svc.Datas);
+					delete svc.Datas["$$conf"];
+					delete svc.Datas["$id"];
+					delete svc.Datas["$value"];
+					delete svc.Datas["$priority"];
+					delete svc.Datas["$resolved"];
+					if (!svc.Datas.Fond) {
+						svc.Datas.Fond = {
+							"Src": "",
+							"Width": 0,
+							"Height": 0
+						};
+					}
+					resolve(svc);
+				})
+			});
+		}
 
-        svc.SetCopy = function(id) {
-            var data = FB.GetObject('templates/'+id);
-            angular.copy( svc.Datas, data);
-            data.$save();
+		svc.SetCopy = function(id) {
+			var data = FB.GetObject('templates/'+id);
+			angular.copy( svc.Datas, data);
+			data.$save();
 		}
 
 		svc.Get = function (id) {
@@ -49,4 +56,4 @@ angular
 		}
 
 		return svc;
-	}]);    
+	}]);
